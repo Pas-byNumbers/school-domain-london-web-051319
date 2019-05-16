01aa2eeef0a0cce7f8fcc2d7da67a8c6e9eb9aef
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-16 06:25:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school_name 
+	
+	def initialize(school_name)
+    @school = school_name
+  end
+  
+  def roster
+  	roster = {}
+  end 
+  
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
+  end
+
+end
